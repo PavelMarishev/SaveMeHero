85def661c202d5e957488095e845b801790378a7
PavelMarishev <karthusss@ya.ru>
2018-01-20 14:01:00 +0200

    Removed unused code

--- SaveMeHero/testScreen.h
+++ SaveMeHero/testScreen.h
@@ -6,28 +6,26 @@ using namespace sf;
 
 class testScreen : public cScreen {
 
-public :
-	virtual int Run(RenderWindow &App); 
-	testScreen(void);
-};
-testScreen::testScreen(void){}
-int testScreen::Run(sf::RenderWindow &App) {
-	Event Event;
-	bool Running = true;
-	CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
-	while (Running)
-	{
-		while (App.pollEvent(Event))
+public:
+	int testScreen::Run(sf::RenderWindow &App) {
+		Event Event;
+		bool Running = true;
+		CircleShape shape(100.f);
+		shape.setFillColor(sf::Color::Green);
+		while (Running)
 		{
-			if (Event.type == sf::Event::Closed)
+			while (App.pollEvent(Event))
 			{
-				return (-1);
+				if (Event.type == sf::Event::Closed)
+				{
+					return (-1);
+				}
 			}
+			App.clear();
+			App.draw(shape);
+			App.display();
 		}
-		App.clear();
-		App.draw(shape);
-		App.display();
+		return (-1);
 	}
-	return (-1);
-}
+
+};
